e2314fe7c0a1bb0e5ceeb91ee9d4065aedeae593
AmulyaJain2004 <jainamulyawin@gmail.com>
2025-03-13 19:04:25 +0530

    Added ML notebooks

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,6 +3,10 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response 
 from .models import StudentSurveyModel
 from .serializers import StudentSurveySerializer
+from django.http import HttpResponse
+import csv
+
+
 
 # Create your views here.
 @api_view(['POST'])
@@ -20,3 +24,11 @@ def get_survey_data(request):
     serializer = StudentSurveySerializer(survey_data, many=True)
     return Response(serializer.data)
 
+def export_survey_data(request):
+    response =  HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="survey_data.csv"'
+    writer = csv.writer(response)
+    writer.writerow([field.name for field in StudentSurveyModel._meta.fields]) # Column headers
+    for student in StudentSurveyModel.objects.all():
+        writer.writerow([getattr(student, field.name) for field in StudentSurveyModel._meta.fields])
+    return response # returning an HTTP response containing a CSV file with survey data which will download automatically
