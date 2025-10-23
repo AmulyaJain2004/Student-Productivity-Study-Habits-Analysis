8a9ef8cade7f902e58bc0939d828310ccd613371
AmulyaJain2004 <jainamulyawin@gmail.com>
2025-10-24 00:43:39 +0530

    Initial Commit

--- backend/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import StudentSurveyModel
-
-class StudentSurveySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = StudentSurveyModel
-        fields = '__all__'
--- backend/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import save_survey_data, get_survey_data, export_survey_data
-
-urlpatterns = [
-    path('save/', save_survey_data),  # POST request to save data
-    path('data/', get_survey_data),  # GET request to retrieve data
-    path('export/', export_survey_data),  # GET request to export data
-]
